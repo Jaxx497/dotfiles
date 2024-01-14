3675e82676a72da67f62db545ed8909b3ebec3e2
Jaxx497 <equus497@gmail.com>
2024-01-13 21:57:19 -0700

    Fixed zsh

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,17 @@
+alias cls='clear'
+alias zrc='source ~/.zshrc && clear'
+
+# Python shortcuts
+alias py=python3
+alias python=python3
+
+# Zypper shortcuts
+alias zyp='sudo zypper in'
+alias zypi='sudo zypper in'
+alias zypr='sudo zypper rm'
+
+# Nvim shortcuts
+alias nv='nvim .'
+alias nvl="NVIM_APPNAME=nvimlite nvim"
+alias nvco='cd ~/.config/nvim && nvim .'
+alias nvz='nvim ~/.zshrc'
---
+++ .zsh/plugins.zsh
@@ -0,0 +1,3 @@
+eval "$(starship init zsh)"
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
